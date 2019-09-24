64f7720290c12e028f52c6ff9349aaa793e206a1
mutongs <mutongf@gmail.com>
2019-09-24 15:51:06 +0800

    删除mvn版本控制

--- src/main/java/com/jidu/aladdin/AladdinApplication.java
+++ src/main/java/com/jidu/aladdin/AladdinApplication.java
@@ -2,6 +2,7 @@ package com.jidu.aladdin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class AladdinApplication {
