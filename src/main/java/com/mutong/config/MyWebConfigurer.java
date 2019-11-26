3865656b56d014799d5442f9ef128bff8e13eb64
mutong <248345431@qq.com>
2019-11-26 16:11:51 +0800

    html的公共变量,开发环境忽略拦截器,添加一个测试环境的配置文件

--- src/main/java/com/mutong/config/MyWebConfigurer.java
+++ src/main/java/com/mutong/config/MyWebConfigurer.java
@@ -2,6 +2,7 @@ package com.mutong.config;
 
 import com.mutong.interceptor.SessionInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,43 +11,40 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
-/**
- * 1.   配置静态资源访问路径
- * 2.   配置拦截器
- */
+/** 1. 配置静态资源访问路径 2. 配置拦截器 */
 public class MyWebConfigurer implements WebMvcConfigurer {
-    @Autowired
-    private SessionInterceptor sessionInterceptor;
+  @Autowired private SessionInterceptor sessionInterceptor;
 
+  @Value("${upload.filePath}")
+  private String filePath;
 
-    // 这个方法是用来配置静态资源的，比如html，js，css，等等
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
-        // 放掉swagger-ui.html,部署了项目名后访问不到的bug
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
-                "classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations(
-                "classpath:/META-INF/resources/webjars/");
-        //配置图片访问路径
-        registry.addResourceHandler("/image/**").addResourceLocations("file:/C:/upload/image/");
+  @Value("${spring.profiles.action}")
+  private String action;
 
+  // 这个方法是用来配置静态资源的，比如html，js，css，等等
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    // 放掉swagger-ui.html,部署了项目名后访问不到的bug
+    registry
+        .addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+    registry
+        .addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    // 配置图片访问路径
+    registry.addResourceHandler("/image/**").addResourceLocations("file:" + filePath);
+  }
+
+  // 这个方法用来注册拦截器，我们自己写好的拦截器需要通过这里添加注册才能生效
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    /** 注册拦截器 addPathPatterns:>:拦截的路径 excludePathPatterns:>:取消拦截的路径 */
+    if (!action.equals("dev")) {
+      registry
+          .addInterceptor(sessionInterceptor)
+          .addPathPatterns("/back/**")
+          .excludePathPatterns("/back/login");
     }
-
-
-    // 这个方法用来注册拦截器，我们自己写好的拦截器需要通过这里添加注册才能生效
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        /**
-         *  注册拦截器
-         *  addPathPatterns:>:拦截的路径
-         *  excludePathPatterns:>:取消拦截的路径
-         */
-        registry.addInterceptor(sessionInterceptor)
-                .addPathPatterns("/back/**")
-                .excludePathPatterns(
-                        "/back/login"
-                );
-    }
+  }
 }
